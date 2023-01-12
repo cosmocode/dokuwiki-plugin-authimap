d418133a347839eccc5c9bf62defaa5cf49e6994
Eduardo Mozart de Oliveira <2974895+eduardomozart@users.noreply.github.com>
2023-01-12 07:46:14 -0300

    Create lang.php

---
+++ lang/en/lang.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * English language file for oauth plugin
+ *
+ * @author Andreas Gohr <andi@splitbrain.org>
+ */
+
+$lang['emailduplicate'] = 'This email is already associated with another user.';
